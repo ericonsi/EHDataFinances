b6b18ddeb791ac81a12cfabfccee5b59f8f1b6b8
ericonsi <ericonsi@yahoo.com>
2026-02-25 14:27:25 -0500

    x

--- R/EHDataFinances.R
+++ R/EHDataFinances.R
@@ -96,8 +96,8 @@ dfExpenses2 <- dfExpenses |>
   ) |>
   ungroup() |>
   mutate(SupercedesTrip=if_else(is.na(SupercedesTrip), 0, SupercedesTrip)) |>
-  mutate(Corrected=0) |>
-  dplyr::filter(year(`Transaction Date`) == year(EHFinances_RetrieveYearAndMonth(Folder)[[1]]) & month(`Transaction Date`) == month(EHFinances_RetrieveYearAndMonth(Folder)[[2]]))
+  mutate(Corrected=0)
+  #dplyr::filter(year(`Transaction Date`) == year(EHFinances_RetrieveYearAndMonth(Folder)[[1]]) & month(`Transaction Date`) == month(EHFinances_RetrieveYearAndMonth(Folder)[[2]]))
 
 liAccounts=list()
 liAccounts[[1]] <- dfExpenses2
