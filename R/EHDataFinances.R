af991cb4ff7cca2d6c13166fc255786ad5b3b397
ericonsi <ericonsi@yahoo.com>
2026-03-06 18:31:50 -0500

    x

--- R/EHDataFinances.R
+++ R/EHDataFinances.R
@@ -578,8 +578,17 @@ EHFinances_CreateBudgetAnalysisDFs <- function(df, df_ytd, Folder) {
   li = list()
   li[[1]] <- EHFinances_BudgetAnalysisDF(df, Folder, ytd=FALSE)
   li[[2]] <- EHFinances_BudgetAnalysisDF(df_ytd, Folder, ytd=TRUE)
-  li[[3]] <- EHFinances_BudgetAnalysisPlot(df, Folder, ytd=FALSE)
-  li[[4]] <- EHFinances_BudgetAnalysisPlot(df_ytd, Folder, ytd=TRUE)
+
+  return(li)
+
+}
+
+#' @export
+EHFinances_CreateBudgetAnalysisPlots <- function(df, df_ytd, Folder) {
+
+  li = list()
+  li[[1]] <- EHFinances_BudgetAnalysisPlot(df, Folder, ytd=FALSE)
+  li[[2]] <- EHFinances_BudgetAnalysisPlot(df_ytd, Folder, ytd=TRUE)
 
   return(li)
 
